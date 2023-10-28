80da9837ebe51c9e572ad0fa111442abffd345b6
duykasama <duykasama@gmail.com>
2023-10-28 11:16:41 +0700

    fetch projects from api

--- src/app/modules/project-list/project-list.component.ts
+++ src/app/modules/project-list/project-list.component.ts
@@ -10,26 +10,37 @@ import axios, {AxiosError} from "axios";
 export class ProjectListComponent implements OnInit {
   faTrash = faTrash
   private url: string = 'http://localhost:10002/projects'
-  ngOnInit(): void {
-    console.log('Start fetching')
-    // fetch(this.url)
-    //   .then(res => res.json())
-    //   .then(jsonRes => console.log(jsonRes))
-    //   .catch(e => console.log(e))
-    this.getData()
+  projects: Project[] = []
+  isLoading = true
+  numbers = [1, 2, 3, 4, 5]
+  async ngOnInit(): Promise<void> {
+    await this.getData()
+    console.log(this.projects)
   }
 
   async getData(): Promise<void> {
     try {
+
       const axiosInstance = axios.create()
       const response = await axiosInstance.post(this.url, {
-        pageSize: 1,
+        pageSize: 10,
         pageIndex: 1
       })
 
-      console.log(response.data)
+      await new Promise(r => setTimeout(r, 1000))
+      this.projects = response.data?.data
+      this.isLoading = false
     }catch (e){
       console.log(e)
     }
   }
+}
+
+interface Project {
+  id: string
+  projectNumber: number
+  name: string
+  status: string
+  customer: string
+  startDate: string
 }
